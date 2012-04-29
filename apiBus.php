db5a5b3e8d3f055965e63e5849b374f777ee8b9b
Francisco Cortés <franklyn.bdn@gmail.com>
2012-04-29 19:29:57 +0200

    basic API formed

--- apiBus.php
+++ apiBus.php
@@ -9,12 +9,20 @@ function URIParser($uriRequest){
 	switch($command){
 		case 'refresh':
 			echo "refresh the busLines API.\n";
+			generateAllBusData("content/global.json");
 			break;
 		case 'generateMarker':
 			echo "generate Markers with CSV data.\n";
+			generateCSVMarkers("content/markersContent.csv");
 			break;
 		case 'markersWithRadius':
 			echo "Marker Round a selected radius.\n";
+			$lat = "41.376765466263";//$uriArray[3];
+			$lng = "2.1520424580862";//$uriArray[4];
+			$radius = "0.4";//$uriArray[5];
+			$inputNameFile = "content/markers.json"//$uriArray[6];
+			$outputNameFile = "content/selectedMarkers.json"//$uriArray[7];
+			calculateMarkersAroundRadius($lat,$lng,$radius,$inputNameFile,$outputNameFile);
 			break;
 	}
 }
