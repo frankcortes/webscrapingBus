f9dc67f1381023c17e93d70dbaf5b4c83c00dd74
Francisco Cortés <franklyn.bdn@gmail.com>
2012-04-29 23:27:05 +0200

    refactoring api Bus

--- apiBus.php
+++ apiBus.php
@@ -4,16 +4,17 @@ require("generateBusData.php");
 
 //Parser the current URI.
 function URIParser($uriRequest){
+	$folder = "content/";
 	$uriArray = explode("/",$uriRequest);
 	$command = $uriArray[2];
 	switch($command){
 		case 'refresh':
 			//echo "refresh the busLines API.\n";
-			generateAllBusData("content/global.json");
+			generateAllBusData($folder."global.json");
 			break;
 		case 'generateMarker':
 			//echo "generate Markers with CSV data.\n";
-			generateCSVMarkers("content/markersContent.csv");
+			generateCSVMarkers($folder."markersContent.csv");
 			break;
 		case 'markersWithRadius':
 			//echo "Marker Round a selected radius.\n";
@@ -21,7 +22,7 @@ function URIParser($uriRequest){
 			$lng = $uriArray[4]; //"2.1520424580862";
 			$radius = $uriArray[5]; //"0.4";
 			$inputNameFile = "markers.json"; //"content/markers.json";
-			calculateMarkersAroundRadius($lat,$lng,$radius,"content/".$inputNameFile);
+			calculateMarkersAroundRadius($lat,$lng,$radius,$folder.$inputNameFile);
 			break;
 	}
 }
