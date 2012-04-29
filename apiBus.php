e3a13598d1d2d173c9924dcf110f89bdad7c683a
Francisco Cortés <franklyn.bdn@gmail.com>
2012-04-29 22:48:13 +0200

    prepare for JSON markers

--- apiBus.php
+++ apiBus.php
@@ -8,20 +8,19 @@ function URIParser($uriRequest){
 	$command = $uriArray[2];
 	switch($command){
 		case 'refresh':
-			echo "refresh the busLines API.\n";
+			//echo "refresh the busLines API.\n";
 			generateAllBusData("content/global.json");
 			break;
 		case 'generateMarker':
-			echo "generate Markers with CSV data.\n";
+			//echo "generate Markers with CSV data.\n";
 			generateCSVMarkers("content/markersContent.csv");
 			break;
 		case 'markersWithRadius':
-			echo "Marker Round a selected radius.\n";
+			//echo "Marker Round a selected radius.\n";
 			$lat = $uriArray[3]; //"41.376765466263";
 			$lng = $uriArray[4]; //"2.1520424580862";
 			$radius = $uriArray[5]; //"0.4";
 			$inputNameFile = "markers.json"; //"content/markers.json";
-			$outputNameFile = $uriArray[7]; //"content/selectedMarkers.json";
 			calculateMarkersAroundRadius($lat,$lng,$radius,"content/".$inputNameFile);
 			break;
 	}
