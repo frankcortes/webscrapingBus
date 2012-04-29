f501a6b2089042f95acba7da41b3189ae0c2ead3
Francisco Cortés <franklyn.bdn@gmail.com>
2012-04-29 19:26:42 +0200

    Api Bus' Skeleton

--- apiBus.php
+++ apiBus.php
@@ -4,8 +4,19 @@ include "generateBusData.php";
 
 //Parser the current URI.
 function URIParser($uriRequest){
-	echo "uri Request: ".$uriRequest;
-	
+	$uriArray = explode("/",$uriRequest);
+	$command = $uriArray[2];
+	switch($command){
+		case 'refresh':
+			echo "refresh the busLines API.\n";
+			break;
+		case 'generateMarker':
+			echo "generate Markers with CSV data.\n";
+			break;
+		case 'markersWithRadius':
+			echo "Marker Round a selected radius.\n";
+			break;
+	}
 }
 
 URIParser($_SERVER["REQUEST_URI"]);
